fc110bef364d165ffafefaf99af76c4b9ed82073
andycnzh <andycnzh@outlook.com>
2017-10-09 15:29:03 +0800

    Add type convert test

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeConvert convert = new TypeConvert();
+            convert.TypeConvertTest();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpTest/TypeConvert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class TypeConvert
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+        }
+
+        public class Teacher : Person
+        {
+            public string Grade { get; set; }
+        }
+
+        public void TypeConvertTest()
+        {
+            Teacher teacher = new Teacher();
+            // type convert is OK
+            Person person = teacher;
+
+            person.Name = "abc";
+
+            // type convert is OK
+            teacher = (Teacher)person;
+
+            Person person2 = new Person();
+            // Exception here: Unable to cast object of type 'Person' to type 'Teacher'.
+            teacher = (Teacher)person2;
+        }
+    }
+}
